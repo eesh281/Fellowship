078874356ea2f78c5f431aea2891a0458d0a417a
Gursheesh <kour.gursheesh281@gmail.com>
2019-12-03 18:35:33 +0530

    adding 5 programs

--- pow2Test.py
+++ pow2Test.py
@@ -1,12 +1,12 @@
 import unittest
 from power2 import power_of_2
 
+
 class MyTestCase(unittest.TestCase):
     def test_pow2(self):
-        self.assertListEqual(power_of_2(6),[1,2,4,8,16,32,64] )
-        self.assertListEqual(power_of_2(2),[1,2,4] )
-        self.assertListEqual(power_of_2(0),[1] )
-       # self.assertListEqual(power_of_2(-1),[0.5] )
+        self.assertListEqual(power_of_2(6), [1, 2, 4, 8, 16, 32, 64])
+        self.assertListEqual(power_of_2(2), [1, 2, 4])
+        self.assertListEqual(power_of_2(0), [1])
 
 
 if __name__ == '__main__':
