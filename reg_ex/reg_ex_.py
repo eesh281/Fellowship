bb427ebb772b591a96c1efefc37ee0c86e159cbd
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-17 19:31:35 +0530

    [Gursheesh Kour].Modified req_ex

---
+++ reg_ex/reg_ex_.py
@@ -0,0 +1,22 @@
+#*********************************************************************************************************
+#@purpose    :regular expression demonstration
+#@file name  :reg_ex_.py
+#@author name:Gursheesh Kour
+#*******************************************************************************************************
+
+#importing module re
+import re
+
+#creating a method to demonstrate regex
+def reg_ex():
+    a = "Hello <<name>>, We have your full name as <<full name>> in our system. your contact number is 91-xxxxxxxxxx."
+    "Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016"
+    p = re.sub("<<name>>", "Gursheesh", a)
+    p = re.sub("<<full name>>", "Gursheesh Kour", p)
+    p = re.sub("xxxxxxxxxx", "9145623060", p)
+    p = re.sub("01/01/2016", "03/12/2019", p)
+    #printing the resultant expression
+    print(p)
+
+#calling the created method
+reg_ex()
