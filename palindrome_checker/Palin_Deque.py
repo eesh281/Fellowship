3e710a2d6929763520191897dbbae9048239c7f0
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-17 15:32:38 +0530

    [Gursheesh Kour].Modified palindrome checker

--- palindrome_checker/Palin_Deque.py
+++ palindrome_checker/Palin_Deque.py
@@ -3,85 +3,10 @@
 # @file    :Palin_Deque.py
 # @author  :GursheeshKour
 # *********************************************************************************************************************************************
+#importing class Deque from utility.py from folder utilss
+from util.utility.py import Deque
 
-
-class node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class deque:
-    def __init__(self):
-        self.front = self.rear = None
-
-    def addRear(self, item):
-        temp = node(item)
-        if self.rear is None:
-            self.rear = self.front = temp
-            return
-        self.rear.next = temp
-        self.rear = temp
-
-    def addFront(self, item):
-        temp = node(item)
-        if self.front is None:
-            self.rear = self.front = temp
-            return
-        newnode = temp
-        newnode.next = self.front
-        self.front = newnode
-
-    def display(self):
-        if self.front is None:
-            print("empty deque")
-        temp = self.front
-        while temp is not None:
-            print(temp.data)
-            temp = temp.next
-
-    def size(self):
-        temp = self.front
-        count = 0
-        while temp.next is not None:
-            count += 1
-            temp = temp.next
-        print("length is: ", count)
-        return count
-
-    def removeFront(self):
-        if self.front is None:
-            print("empty")
-        self.front = self.front.next
-
-    def removeRear(self):
-        if self.front is None:
-            print("empty")
-        if self.front.next is None:
-            self.front = None
-            return None
-        second_last = self.front
-        while second_last.next.next:
-            second_last = second_last.next
-        second_last.next = None
-        return self.front
-
-    def is_Empty(self):
-        temp = self.front
-        if temp.next == 0:
-            print("Empty")
-
-    def reverse(self):
-        last = None
-        temp = self.front
-        while temp.next is not None:
-            prev = temp.next
-            temp.next = last
-            last = temp
-            temp = prev
-        self.front = last
-
-
+#method to check whether string is palindrome or not
 def palindrome(string):
     rev = string[::-1]
     if string == rev:
@@ -89,8 +14,10 @@ def palindrome(string):
     return False
 
 
+#input a string to be checked 
 string = input("enter a string: ")
-d = deque()
+#create an object of class Deque
+d = Deque()
 for word in string:
     d.addRear(word)
 d.display()
