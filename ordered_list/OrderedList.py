61ece53563652effadc63f2bb29c3bc12d5c2f4f
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-17 15:22:34 +0530

    [Gursheesh Kour].Modified ordered list

--- ordered_list/OrderedList.py
+++ ordered_list/OrderedList.py
@@ -3,13 +3,16 @@
 # @file    :Ordered_List.py
 # @author  :GursheeshKour
 # *********************************************************************************************************************************************
-from util.utility.py import Linked_List
+#importing Linked_List from utility.py from package util
+from util.utility import Linked_List
 
-
+#method to open file
 def openfile():
     file = open("Num_File.txt", "r+")
     x = file.read()
+    #creating an object of Linked_List class
     li = Linked_List()
+    #adding words to linkedlist
     for word in x.split():
         li.add(word)
     li.display()
@@ -23,6 +26,6 @@ def openfile():
     file.write(s_word)
     file.close()
 
-
+#calling the method created
 openfile()
 
