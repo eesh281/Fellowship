b29e7a35ad46473115ecf8d04cc64bcf27424ac5
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-17 18:39:39 +0530

    [Gursheesh Kour].Modified stop watch

--- stop_watch/StopWatch.py
+++ stop_watch/StopWatch.py
@@ -1,10 +1,22 @@
+#importing time module
 import time
 
-start=input("press enter to start the timer")
+#taking the instance to start from user
+start = input("press enter to start the timer")
 print("timer on")
+
+#taking current time from system
 begin=time.time()
+
 endtimer=input("press enter to end the timer")
+
+#taking end time from system
 end=time.time()
+#finding time elapsed
 elapsed=end-begin
+
+#converting it into int
 elapsed=int(elapsed)
+
+#printing elapsed seconds
 print("The time elapsed is",elapsed,"seconds")
