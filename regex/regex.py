81263ab1cb4d3321e18dd8d2c229eb56e2828a72
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-19 14:45:39 +0530

    [Gursheesh Kour].Modified regex program

--- regex/regex.py
+++ regex/regex.py
@@ -1,7 +1,34 @@
+#*****************************************************************************************************
+#@purpose: to implement regex
+#@file:regex.py
+#@author:Gursheesh Kour
+#*****************************************************************************************************
+
+#importing regex module
+import re
+#importing datetime module
+import datetime
+
+#creating a class named Reg_ex for implementing regular expression
 class Reg_ex:
 
+    #method created
     def regex(self):
-        with open("/home/user/Desktop/newfolder/Fellowship/regex/reg_text.txt", "r+") as file:
-            x = file.read()
-            print(x)
+        #file opened with read and writing permission
+        file = open("/home/user/Desktop/newfolder/Fellowship/regex/reg_text.txt", "r+")
+        x = file.read()
+        #using regex function sub
+        data = re.sub("[<]{2}[a-zA-Z]{4}[>]{2}", "Gursheesh", x) 
+        data = re.sub("[<]{2}[a-z]{8}[>]{2}", "Gursheesh Kour" , data)
+        data = re.sub("[x]{10}", "9419624315" , data)
+        today = str(datetime.date.today())
+        data = re.sub("01/01/2016",today , data)
+        #writing into the file
+        file.write(data)
+        file.write("\n")
+        #closing file
+        file.close()
 
+
+obj = Reg_ex()
+obj.regex()
