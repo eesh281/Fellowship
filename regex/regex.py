ef974d17fcb4b64b8643953542251a00b680321f
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-19 09:52:59 +0530

    [Gursheesh Kour].Added regex program

---
+++ regex/regex.py
@@ -0,0 +1,7 @@
+class Reg_ex:
+
+    def regex(self):
+        with open("/home/user/Desktop/newfolder/Fellowship/regex/reg_text.txt", "r+") as file:
+            x = file.read()
+            print(x)
+
