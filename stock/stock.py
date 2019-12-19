bf256fe0c363d9afddaaf51a23886a900963209e
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-19 19:19:36 +0530

    [Gursheesh Kour].Completed stock program

--- stock/stock.py
+++ stock/stock.py
@@ -4,5 +4,22 @@
 #@author:Gursheesh Kour
 #*****************************************************************************************************
 
+import json 
+from stock_portfo import Cal_Stock
+
 class Stock:
-    
+
+    def stock_open(self):
+        file = open("/home/user/Desktop/newfolder/Fellowship/stock/stock_json.json","r")
+        stock_dict = json.load(file)
+        stock_arr = stock_dict["Stock_name"]
+        return stock_arr
+
+  
+obj1 = Stock()
+a = obj1.stock_open()
+print(a)
+obj = Cal_Stock()
+obj.valueof_each(a)
+obj.total_value(a)
+            
--- stock/stock_portfo.py
+++ stock/stock_portfo.py
@@ -3,11 +3,21 @@
 #@file:stock_portfo.py
 #@author:Gursheesh Kour
 #*****************************************************************************************************
+import json
 
-class Each_Stock:
-    def valueof_each(self):
+class Cal_Stock:
+    
+    def valueof_each(self, stock_arr):
+        each_price = 0
+
+        for key in stock_arr:
+            each_price =  key["numof_share"] * key["price"]
+        print("price of each stock is: ", each_price)
         
+    def total_value(self, stock_arr):
+        total_price = 0
 
+        for key in stock_arr:
+            total_price = total_price + key["price"]
+        print("price of total stock is: ", total_price)
 
-
-#def total_value
