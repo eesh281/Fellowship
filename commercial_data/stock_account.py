ff058167fa27f354fcb79e9415e6d7f403ad19a1
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-23 12:54:54 +0530

    [Gursheesh Kour].Modified commercial data program

--- commercial_data/stock_account.py
+++ commercial_data/stock_account.py
@@ -8,18 +8,37 @@ class StockAccount:
      
         #user_arr = data_dict["Users"]
         share_arr = data_dict["Shares"]
-        return share_arr
+        x = input("Type: 0 for HCL \n Type: 1 for TATA \n Type: 2 for BRIDGELABZ \n")
 
-    def update_json(self, share_arr):
-        result_list = [ sub["amount"] for sub in share_arr ]
+        if x == 0:
+            print("hello")
+            share_amount = share_arr[2]
+            final_amount = share_amount["amount"]
+            print(final_amount)
+            return final_amount
+        if x == 1:
+            share_amount = share_arr[1]
+            final_amount = share_amount["amount"]
+            print(final_amount)
+            return final_amount
+        if x == 2:
+            share_amount = share_arr[2]
+            final_amount = share_amount["amount"]
+            print(final_amount)
+            return final_amount
+        
+        
+
+        
+
+    def update_json(self, final_amount):
+       
         input_amount = int(input("enter the amount for the shares: "))
-        for item in result_list:
-            leftover = item - input_amount
-            print(leftover)
-            return leftover
+        substract = final_amount - input_amount
+        print(substract)
+        return substract 
 
-
-
+    
 
 obj = StockAccount()
 a= obj.openning_file()
@@ -32,9 +51,8 @@ if __name__ == '__main__' :
     if (inp == "y") or (inp == "Y"):
         obj.openning_file()
         file2 = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/file.json", "w") 
-        data = json.dumps(data)
         json.dump(data, file2, indent=4)
-        #file2.write(data)
+        
     else:
         f = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/company_data.json","a+")
         name = input("Enter the Name: ")
@@ -42,7 +60,7 @@ if __name__ == '__main__' :
         number = int(input("Enter your Mobile Number: "))
         address = (input("Enter the Address: "))
         writting = f.write(' [{'+'     "Name" : "'+ name +'",\n' + '        "Age"  : "'+str(age)+'",\n'+'       "Ph.No" : "'+str(number)+'",\n'+'"Address" : "'+str(address)+'" }]')
-        print("The Data has been Stored Successfully in 'newUser.json' file. ")
+        print("The Data has been Stored Successfully in the file. ")
 
 
 
