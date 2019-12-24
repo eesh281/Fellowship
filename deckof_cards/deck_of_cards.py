545d01e44c7170d86d4e6ddfd13d8d1e8be9d4b1
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-24 15:17:28 +0530

    [Gursheesh Kour].Modified deck of cards program

--- deckof_cards/deck_of_cards.py
+++ deckof_cards/deck_of_cards.py
@@ -39,19 +39,24 @@ class Card(object):
         return format1
 
 
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
 
-    # Shuffle the deck
-    def shuffle(self, num=1):
-        length = len(self.cards)
-        for _ in range(num):
-            # This is the fisher yates shuffle algorithm
-            for i in range(length-1, 0, -1):
-                randi = random.randint(0, i)
-                if i == randi:
-                    continue
-                self.cards[i], self.cards[randi] = self.cards[randi], self.cards[i]
-            # You can also use the build in shuffle method
-            # random.shuffle(self.cards)
+    # Display all cards in the deck
+    def display(self):
+        for card in self.cards:
+            print (card.display())
+
+    # Generate 52 cards
+    def build(self):
+        self.cards = []
+        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+            for val in range(1,14):
+                self.cards.append(Card(suit, val))
+
+    
 
     # Return the top card
     def deal(self):
