8af70864df4ac43eb546db01a1b1f7544fee0fe1
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-24 15:07:46 +0530

    [Gursheesh Kour].Modified deck of cards program

--- deckof_cards/player_card.py
+++ deckof_cards/player_card.py
@@ -1,15 +1,23 @@
+from deck_of_cards import Card
+from deck_of_cards import Deck
 
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
 
-# Test making a Card
-# card = Card('Spades', 6)
-# print card
+    def sayHello(self):
+        print ("I am {}".format(self.name))
+        return self
 
-# Test making a Deck
-myDeck = Deck()
-myDeck.shuffle()
-# deck.display()
+    # Draw n number of cards from a deck
+    # Returns true in n cards are drawn, false if less then that
+    def draw(self, deck, num=1):
+        for _ in range(num):
+            card = deck.deal()
+            if card:
+                self.hand.append(card)
+            else: 
+                return False
+        return True
 
-obj = Player("Gursheesh")
-obj.sayHello()
-obj.draw(myDeck, 5)
-obj.displayHand()
