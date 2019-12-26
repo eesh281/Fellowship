8506b13fdbf2da354f839929a84d06cf1dcbe3d3
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-26 18:55:20 +0530

    [Gursheesh Kour].Refractor.refracted commercial data program

---
+++ CommercialData/stock_account.py
@@ -0,0 +1,117 @@
+#******************************************************************************************************
+#@purpose:demonstrating stock market using json
+#@file   :stock_account.py
+#@author :Gursheesh Kour
+#*******************************************************************************************************
+
+try:
+    #importing json module
+    import json
+except ImportError:
+    print("Error while Importing")
+
+class StockAccount:
+
+    #getting details from json file
+    def stock_details(self):
+        
+        try:
+            #openning a file to read 
+            file = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/client_data.json","r")
+       
+            #loading data from json file
+            data_dict= json.load(file)
+        except FileNotFoundError:
+            print("Error while loading file")
+        
+
+        x = int(input("\n0 for HCL \n1 for TATA \n2 for BRIDGELABZ"))
+        
+        #comparing the input value for running various conditions
+        if x == 0:
+            
+            for data in data_dict["Shares"]:
+                
+                if data['share'] == "HCL":
+                    
+                    input_amount=int(input("enter amount"))  
+                    data['amount'] -= input_amount
+                    
+                    json_file = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/client_data.json", "w")
+                    json.dump(data_dict, json_file)
+       
+        if x == 1:
+            
+            for data in data_dict["Shares"]:
+               
+                if data['share'] == "TATA": 
+                    
+                    input_amount=int(input("enter amount"))
+                    data['amount'] -= input_amount
+                    
+                    json_file = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/client_data.json", "w")
+                    json.dump(data_dict, json_file)
+        
+        if x == 2:
+            
+            for data in data_dict["Shares"]:
+                
+                if data['share'] == "BRIDGELABZ":
+                    
+                    input_amount=int(input("enter amount"))
+                    data['amount'] -= input_amount
+                    
+                    json_file = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/client_data.json", "w")
+                    json.dump(data_dict, json_file)
+    #closing the file after performing the operation
+        file.close()
+
+if __name__ == '__main__' :
+
+    inp = input(" Are you an existing user(y/n):")
+    file=open("/home/user/Desktop/newfolder/Fellowship/commercial_data/client_data.json","r")
+    #loading data from json file
+    data_dict= json.load(file)
+    user_arr =data_dict["Users"]
+
+    #condition to check whether the costumer is registered or not
+    if (inp == "y") or (inp == "Y"):
+        
+        costumer_key = input("enter costumer id - 'registered name': ")
+        
+        for data in data_dict["Users"]:
+           
+            if costumer_key == data["name"]:
+               
+                print("welcome back!")
+                obj1 = StockAccount()
+                obj1.stock_details()
+            
+            
+    else:
+        
+        #appending a new user into the file
+        f = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/newUser.json","w")
+        dict1 = {}
+       
+        dict1["name"] = input("Enter the Name: ")
+        dict1["age"] = int(input("Enter the Age: "))
+        dict1["ph num"] = int(input("Enter your Mobile Number: "))
+        dict1["amount"] = int(input("Enter the Amount for Shares: "))
+        print(dict1)
+        
+        #data is being added to the json file
+        json.dump(dict1, f)
+        
+        print("The Data has been Stored Successfully in 'newUser.json' file. ")
+    #closing the file after use
+    file.close()
+
+
+
+
+
+
+
+
+    
