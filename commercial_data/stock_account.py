4bd9d6ed906594a731871be102b8216e5f20fa8d
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-26 16:13:46 +0530

    [Gursheesh Kour].Modify.Modified commercial data

--- commercial_data/stock_account.py
+++ commercial_data/stock_account.py
@@ -88,11 +88,10 @@ if __name__ == '__main__' :
                 obj1.stock_details()
             
             
-      
     else:
         
         #appending a new user into the file
-        f = open("newUser.json","w")
+        f = open("/home/user/Desktop/newfolder/Fellowship/commercial_data/newUser.json","w")
         dict1 = {}
        
         dict1["name"] = input("Enter the Name: ")
