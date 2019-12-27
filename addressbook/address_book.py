f70792e4f4ddc33d784733af165edbc550472a3e
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-27 10:46:51 +0530

    [Gursheesh Kour].Refractor.refracted address book program

---
+++ addressbook/address_book.py
@@ -0,0 +1,185 @@
+try:
+    import json
+except ImportError:
+    print("import error")
+
+
+# address book class is created
+class AddressBook(): 
+    
+    # here file is initialised with file name
+    def __init__(self, filename):  
+
+        with open(filename) as f:
+            # json file is loaded
+            self.data = json.load(f)  
+
+    # this function is used for added data to the json file
+    def addRecord(self):  
+        
+        while True:
+
+            try:
+                
+                cities = ["jammu", "chhatisgarh", "bhopal" ]
+                states = ["j&k", "chhatisgarh", "bhopal"]
+
+                first_name = input("enter your first name :")
+                
+                if first_name.isalpha() is False:
+                    print("enter vaild name ")
+                    continue
+
+                last_name = input("enter your last name :")
+                
+                if last_name.isalpha() is False:
+                    print("length of last name should be less than 26")
+                    continue
+
+                address = input("enter your 1st and 2nd line of address :")
+                
+                if len(address) >= 70:
+                    print("length of address should be less than 70")
+                    continue
+
+                for i in cities:
+                    print( i, end=" ")
+                
+                city = input("\nenter the city name from above list :")
+                
+                while True:
+                    flag = 0
+                    
+                    try:
+                        
+                        for i in cities:
+                            
+                            if city == i:
+                                print("done")
+                                flag = 1
+                                break
+                            else:
+                                return
+                        
+                        if flag == 1:
+                            break
+                    
+                    except ValueError:
+                        print("error")
+
+                state = input("enter the state name :")
+
+                zipcode = int(input("enter the zip code :"))
+                
+                if len(str(zipcode)) >= 7:
+                    print("length of input should be less than 7")
+                    continue
+
+                phone_number = int(input("enter the full mobile number"))
+                
+                # if phone_number <= 916000000000 or phone_number >= 920000000000:
+                #     print("enter vaild number starting from 91")
+                #     continue
+
+                # dic is used for storing user input data
+                dic = {"first_name": first_name,
+                       "last_name": last_name,
+                       "address": address,
+                       "city": city,
+                       "state": state,
+                       "zipcode": zipcode,
+                       "phone_number": phone_number}
+                
+                print("user data added successfully")
+
+                data = self.data
+                # user data is added to the file
+                data.append(dic)  
+
+                # now data and input data is called in main file
+                print(dic)  
+                break
+            
+            except ValueError:
+                print("check user input")
+
+    # this function is used for deleting data in the json file
+    def delete(self):  
+
+        # input is used for deleting data
+        datadelete = input("\nname of the person you want to delete from the address book :")
+        print(datadelete, "is deleted from address book ")
+
+        name = []
+        
+        for i in range(len(self.data)):
+            name.append(self.data[i]["first_name"])
+
+        # index is used for keeping track of the index where we have to delete the data
+        index = -1  
+        # para is used for transversing through the data
+        for para in self.data:  
+            index += 1
+
+            # if condition is used for checking user input
+            if datadelete == para["first_name"]:  
+                # here data is deleted if data  matches
+                del self.data[index]  
+                # index is returned for future
+                return index  
+
+        print(datadelete, " is deleted from address book ")
+
+    # this function is used for printing data
+    def printData(self):  
+        
+        data = self.data
+
+        # for loop is used transversing values through data
+        for i in range(len(data)):  
+            
+            for j in data[i].values():
+                print(j)
+            print()
+
+    # sort function is used for sorting the data in json file
+    def sort(self):  
+
+        array = []
+        data = self.data  
+
+        # first name is appended in array
+        for i in range(len(self.data)): 
+            array.append((self.data[i]["first_name"]))
+             #names are sorted
+        name_sort = sorted(array) 
+
+        # this empty array is used for storing sorted file
+        sorteddata = []  
+
+        # nested loop is used for matching sorted names to file
+        for i in name_sort:  
+            
+            for j in range(len(data)):
+                
+                if i == data[j]["first_name"]:
+                    # if name matches then data is appended
+                    sorteddata.append(data[j])  
+
+        # now we have swapped data with sorted data
+        self.data = sorteddata  
+        # here we have return sorted data
+        return sorteddata  
+
+    def dumping(self, filename):  
+
+        with open('/home/user/Desktop/newfolder/Fellowship/addressbook/details.json', 'w') as f:
+            json.dump(self.data, f, indent=2)
+
+    def onlyNames(self): 
+         # printing names from the file
+        data = self.data
+        for i in range(len(data)):
+
+             print( (data[i]["first_name"]), end=" ")
+
---
+++ addressbook/adress_management.py
@@ -0,0 +1,63 @@
+try:
+    import json
+    from address_book import AddressBook
+except ImportError:
+    print("import error")
+
+def addressbook_management():
+    while True:
+        address = AddressBook("/home/user/Desktop/newfolder/Fellowship/addressbook/details.json")
+
+         # try function is used for catching error if any
+        try: 
+           
+            # user input is taken
+            userdata = int(input("number of data you want to delete or add :"))
+            if userdata>=10:
+                print("enter the number below 10")
+                continue
+            for i in range(userdata):
+
+                user = int(input("""\npress 1 to add\npress 2 to delete\
+    \npress 3 to  exit \npress 4 to print in mailing format
+                                   \nuser input :"""))
+                if user>=5:
+                    print("enter below 5")
+                    continue
+
+                # if user input is 1 then a new data will be ad`ded to the address book
+                if user == 1:
+
+                    print(address.addRecord())   
+
+                # if user input is 2 then one of the data will be deleted
+                if user == 2:
+
+                    address.onlyNames()   
+                    address.delete()   
+
+                # if user input is 3  then address book will be sorted and dump back to the json file
+                if user == 3:
+
+                    print("program ended")
+                    return
+
+                # if user inputs 4 then data is printed in mailing format
+                elif user == 4:
+
+                    address.printData()   
+
+                # else below statement will be printed
+
+                address.sort() 
+                print("\ndata is sorted which can be seen in json file ")
+                address.dumping("/home/user/Desktop/newfolder/Fellowship/addressbook/details.json")
+                break
+
+        except ValueError:      
+            print("check the input")
+
+
+
+if __name__ == '__main__':
+    addressbook_management()
