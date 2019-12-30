6bc75b1080f67dadbea16977085aa987264b227e
GursheeshKour <kour.gursheesh281@gmail.com>
2019-12-30 16:40:53 +0530

    [Gursheesh Kour]Refractor.refracted chatapp

--- chatApp/AuthenticationSystem/AuthenticationSystem/settings.py
+++ chatApp/AuthenticationSystem/AuthenticationSystem/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loginSystem',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 
 ROOT_URLCONF = 'AuthenticationSystem.urls'
 
