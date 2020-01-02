a15892fd35c906dadaf1f3ec30c7eb3e6123881e
Gursheesh <kour.gursheesh281@gmail.com>
2020-01-02 21:56:30 +0530

    [Gursheesh Kour]Refractor.refracted project file

--- project/project/snippets/views.py
+++ project/project/snippets/views.py
@@ -1,9 +1,16 @@
+import datetime
+import json
+import django
+import jwt
+from django.conf import setting
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
+from 
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from 
 from django.contrib import messages
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -42,26 +49,95 @@ class Login(GenericAPIView):
 
 
 
+# class Registrations(GenericAPIView):
+
+#     serializer_class = RegistrationSerializer
+    
+#     def get(self, request):
+#         return render(request, 'registration.html')
+        
+#     def post(self, request):
+#             form = UserCreationForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 username = form.cleaned_data.get('username')
+#                 raw_password = form.cleaned_data.get('password')
+#                 user = authenticate(username=username, password=raw_password)
+#                 login(request, user)
+#                 return redirect('home')
+#         else:
+#             form = UserCreationForm()
+#         return render(request, 'registration.html', {})
+
 class Registrations(GenericAPIView):
 
-    serializer_class = RegistrationSerializer
-    
+    serializer_class = UserSerializer
+
     def get(self, request):
-        return render(request, 'registration.html')
-        
+        return render(request, 'user/registration.html')
+
     def post(self, request):
-        if request.method == 'POST':
-            form = UserCreationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                username = form.cleaned_data.get('username')
-                raw_password = form.cleaned_data.get('password')
-                user = authenticate(username=username, password=raw_password)
-                login(request, user)
-                return redirect('home')
+
+        username = request.data['username']
+        email = request.data['email']
+        password = request.data['password']
+
+
+        smd = {
+            'success': False,
+            'message': "not registered yet",
+            'data': [],
+        }
+
+        try:
+            validate_email(email)
+        except Exception as e:
+            smd['message'] = "please enter vaild email address"
+            logger.error("error: %s while as email entered was not a vaild email address", str(e))
+            return HttpResponse(json.dumps(smd), status=400)
+
+        if username == "" or email == "" or password == "":
+            smd['message'] = "one of the details missing"
+            logger.error("one of the details missing logging in")
+            return HttpResponse(json.dumps(smd), status=400)
+
+        elif User.objects.filter(email=email).exists():
+            smd['message'] = "email address is already registered "
+            logger.error("email address is already registered  while logging in")
+            return HttpResponse(json.dumps(smd), status=400)
+
         else:
-            form = UserCreationForm()
-        return render(request, 'registration.html', {})
+            try:
+                user_created = User.objects.create_user(username=username, email=email, password=password, is_active=True)
+                user_created.save()
+
+                if user_created is not None:
+                    token = token_activation(username, password)
+                    url = str(token)
+                    surl = get_surl(url)
+                    z = surl.split("/")
+
+                    mail_subject = "Activate your account by clicking below link"
+                    mail_message = render_to_string('user/email_validation.html', {
+                        'user': user_created.username,
+                        'domain': get_current_site(request).domain,
+                        'surl': z[2]
+                    })
+                    recipient_email = user_created.email
+                    email = EmailMessage(mail_subject, mail_message, to=[recipient_email])
+                    email.send()
+                    smd = {
+                        'success': True,
+                        'message': 'please check the mail and click on the link  for validation',
+                        'data': [token],
+                    }
+                    logger.info("email was sent to %s email address ", username)
+                    return HttpResponse(json.dumps(smd), status=201)
+            except Exception as e:
+                smd["success"] = False
+                smd["message"] = "username already taken"
+                logger.error("error: %s while loging in ", str(e))
+                return HttpResponse(json.dumps(smd), status=400)
 
 def activate(request, surl):
     
